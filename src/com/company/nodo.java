01f53803620365332bd501eb7805637e09acee4b
Jasmin <tamara.88jasmin@gmail.com>
2017-11-08 14:31:12 -0600

    Ejemplo de operaciones con pilas

---
+++ src/com/company/nodo.java
@@ -0,0 +1,37 @@
+package com.company;
+
+/**
+ * Created by Chovibys on 8/11/2017.
+ */
+public class nodo {
+
+        private int valor;
+        private nodo prox;
+
+        public nodo()  {
+            this.valor =  0;
+            this.prox = null;
+        }
+
+        public nodo(int v){
+            this.valor = v;
+            this.prox = null;
+        }
+
+        public void setValor (int v){
+            valor = v;
+        }
+
+        public void setProx(nodo p) {
+            prox = p;
+        }
+
+        public int getValor() {
+            return valor;
+        }
+
+        public nodo getProx() {
+            return prox;
+        }
+    }
+
---
+++ src/com/company/pilas.java
@@ -0,0 +1,58 @@
+package com.company;
+
+import com.company.nodo;
+
+public class pilas {
+    private nodo top;
+
+    public pilas() {
+        top = null;
+    }
+
+    public boolean vacia() {
+        return (top == null);
+    }
+
+    public void push(int valor) {//insertar
+        nodo nuevoNodo;
+        if (vacia())
+            top = new nodo(valor);
+        else {
+            nuevoNodo = new nodo(valor);
+            nuevoNodo.setProx(top);
+            top = nuevoNodo;
+        }
+    }
+
+    public void peek() {//mostrar
+        nodo temp = top;
+        if (temp != null) {
+            System.out.println("La pila es:  ");
+            while (temp != null) {
+                System.out.println(temp.getValor());
+                temp = temp.getProx();
+            }
+        } else
+            System.out.println("Pila vacia.");
+
+    }
+
+
+    public void cima() {// buscar
+        if (!vacia())
+            System.out.println("Cima: " + top.getValor());
+        else
+            System.out.println("LA pila esta vacia ");
+
+    }
+
+    public void pop() {// extraer
+        if (!vacia()) {
+            System.out.println("Dato extraido: " + top.getValor());
+            top = top.getProx();
+        } else
+            System.out.println("La pila esta vacia");
+    }
+}
+
+
